6f761fe24c076140e72d4b4ebdb50e223cb05545
Heval98 <jmheval@gmail.com>
2020-08-25 15:53:42 -0500

    Configuring React

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//Import this after installing reactstrap packages
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
