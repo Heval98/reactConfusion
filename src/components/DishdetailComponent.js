742b3b99df3ca754a2a502ee87b7cf01232330ef
Heval98 <jmheval@gmail.com>
2020-08-28 16:22:39 -0500

    Single page application

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     function RenderDish({dish}) {
@@ -21,28 +21,29 @@ import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
         if (comments == null) {
             return <div></div>
         }
-        const commentsConst = comments.map(comment => {
-            return (
-                <ul key={comment.id} className='list-unstyled'>
-                    <li>
-                        <p>{comment.comment}</p>
-                    </li>
-                    <li>
-                        <p>-- {comment.author} ,
-                        {new Intl.DateTimeFormat('en-US', {
-                                year: 'numeric',
-                                month: 'short',
-                                day: '2-digit'
-                            }).format(new Date(comment.date))}
-                        </p>
-                    </li>
-                </ul>
-            )
-        });
         return (
             <div className='col-12 col-md-5 m-1'>
                 <h4> Comments </h4>
-                {commentsConst}
+                <ul className='list-unstyled'>
+                    {comments.map((comment) => {
+                        return (
+                            <div key={comment.id}>
+                                <li>
+                                    <p>{comment.comment}</p>
+                                </li>
+                                <li>
+                                    <p>-- {comment.author} ,
+                                    {new Intl.DateTimeFormat('en-US', {
+                                            year: 'numeric',
+                                            month: 'short',
+                                            day: '2-digit'
+                                        }).format(new Date(comment.date))}
+                                    </p>
+                                </li>
+                            </div>
+                        );
+                    })}
+                </ul>
             </div>
         );
     }
@@ -57,9 +58,23 @@ import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
         }else{
             return (
                 <div className='container'>
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to='/menu'>Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>
+                                {props.dish.name}
+                            </BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
                     <div className='row'>
                         <RenderDish dish={props.dish} />
-                        <RenderComments comments={props.dish.comments} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
                 
