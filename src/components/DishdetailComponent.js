8ac224c7e6c158df9248fb53d766d20412a9f452
Heval98 <jmheval@gmail.com>
2020-09-15 11:50:41 -0500

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -30,7 +30,7 @@ class CommentForm extends Component {
 
     handleComments(values){
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.raiting, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.raiting, values.author, values.comment);
     }
 
 
@@ -121,7 +121,7 @@ class CommentForm extends Component {
         );
     }
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
         if (comments == null) {
             return <div></div>
         }
@@ -148,7 +148,7 @@ class CommentForm extends Component {
                         );
                     })}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
     }
@@ -193,7 +193,7 @@ class CommentForm extends Component {
                     <div className='row'>
                         <RenderDish dish={props.dish} />
                         <RenderComments comments={props.comments}
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id} />
                     </div>
                 </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 
 //This arrow function is to gather information
@@ -23,7 +23,7 @@ const mapStateToProps = state => {
 
 //This function is to set information in a new state
 const mapDispatchToProps = (dispatch) => ({
-  addComment: (dishId, raiting, author, comment) => dispatch(addComment(dishId, raiting, author, comment)),
+  postComment: (dishId, raiting, author, comment) => dispatch(postComment(dishId, raiting, author, comment)),
   fetchDishes: () => {dispatch(fetchDishes())},
   resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
   fetchComments: () => {dispatch(fetchComments())},
@@ -65,7 +65,7 @@ class Main extends Component{
           errMess={this.props.dishes.errMess}
           comments={this.props.comments.comments.filter((comment)  => comment.dishId  === parseInt(match.params.dishId, 10))}
           commentsErrMess={this.props.comments.errMess}
-          addComment={this.props.addComment}
+          postComment={this.props.postComment}
         />
       );
     }
